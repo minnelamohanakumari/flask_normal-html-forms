1b990b1d2a555e100d89a5f4ae4539f97c8690ab
minnelamohanakumari <minnelamohanakumari18gmail.com>
2023-05-30 17:39:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+FAI=Flask(__name__)
+
+
+@FAI.route('/data',methods=['GET','POST'])
+def data():
+    if request.method=='POST':
+        form_data=request.form
+        print(form_data) 
+        return form_data['un']
+    return render_template('data.html')
+
+if __name__=='__main__':
+    FAI.run(debug=True)
